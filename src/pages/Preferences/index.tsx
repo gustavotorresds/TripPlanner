9491fac1904a9791625e89bf5d34747412562600
Gustavo Torres <gustavotorresds@gmail.com>
2023-06-11 23:47:43 -0700

    Set initial implementation of search

--- src/pages/Preferences/index.tsx
+++ src/pages/Preferences/index.tsx
@@ -10,6 +10,7 @@ import {
   Button,
   TouchableOpacity,
   TextInput,
+  FlatList,
 } from 'react-native';
 
 import {
@@ -19,16 +20,30 @@ import {
   cityFromString,
 } from '../../utils';
 
+
+const cityOptions = ['Barcelona', 'Miami', 'Lisbon', 'Rome', 'Rio de Janeiro'];
+
+const CityOption = ({ city }) => {
+  return (
+    <TouchableOpacity style={styles.cityOption}>
+      <Text>{city}</Text>
+    </TouchableOpacity>
+  );
+}
+
 const PreferencesScreen = ({ route, navigation }) => {
   const { trip } = route.params;
 
   const [activeField, setActiveField] = useState(undefined);
+  const [isFocused, setIsFocused] = useState(false);
   const [tempTrip, setTempTrip] = useState(trip);
 
   return (
     <View style={styles.container}>
       <TouchableOpacity
-        onPress={() => setActiveField('from') }
+        onPress={() => {
+          setActiveField('from');
+        }}
         style={styles.card}
       >
         <View style={styles.cardHeader}>
@@ -36,79 +51,91 @@ const PreferencesScreen = ({ route, navigation }) => {
           <Text style={styles.cardDescription}>{cityFromString(tempTrip)}</Text>
         </View>
         {activeField == 'from' ?
-          <TextInput
-            style={styles.cityInput}
-            value={tempTrip.cityFrom}
-            onChangeText={(newText) => setTempTrip({...tempTrip, cityFrom: newText})}
-          />
+          <>
+            <TextInput
+              style={styles.cityInput}
+              value={tempTrip.cityFrom}
+              onFocus={() => setIsFocused(true)}
+              onChangeText={(newText) => setTempTrip({...tempTrip, cityFrom: newText})}
+            />
+            <FlatList
+              data={cityOptions.filter(c => c.startsWith(tempTrip.cityFrom))}
+              renderItem={({ item }) => <CityOption city={item} />}
+            />
+          </>
           :
           undefined
         }
       </TouchableOpacity>
 
-      <TouchableOpacity
-        onPress={() => setActiveField('destinations') }
-        style={styles.card}
-      >
-        <View style={styles.cardHeader}>
-          <Text style={styles.cardTitle}>Where to?</Text>
-          <Text style={styles.cardDescription}>{destinationCitiesString(tempTrip)}</Text>
-        </View>
+      {activeField === 'from' && isFocused ?
+      null :
+      <>
+        <TouchableOpacity
+          onPress={() => setActiveField('destinations') }
+          style={styles.card}
+        >
+          <View style={styles.cardHeader}>
+            <Text style={styles.cardTitle}>Where to?</Text>
+            <Text style={styles.cardDescription}>{destinationCitiesString(tempTrip)}</Text>
+          </View>
 
-        {activeField == 'destinations' ?
-          <View>
-            {tempTrip.destinationCities.map((dC, idx) =>
+          {activeField == 'destinations' ?
+            <View>
+              {tempTrip.destinationCities.map((dC, idx) =>
+                <TextInput
+                  style={styles.cityInput}
+                  value={dC.city}
+                  onChangeText={(newText) => setTempTrip({
+                    ...tempTrip,
+                    destinationCities: [
+                      ...tempTrip.destinationCities.slice(0, idx),
+                      { ...tempTrip.destinationCities[idx], city: newText },
+                      ...tempTrip.destinationCities.slice(idx + 1),
+                    ]
+                  })}
+                  key={`dC-${idx}`}
+                />
+              )}
               <TextInput
                 style={styles.cityInput}
-                value={dC.city}
+                placeholder="Add destination"
+                value=""
                 onChangeText={(newText) => setTempTrip({
                   ...tempTrip,
                   destinationCities: [
-                    ...tempTrip.destinationCities.slice(0, idx),
-                    { ...tempTrip.destinationCities[idx], city: newText },
-                    ...tempTrip.destinationCities.slice(idx + 1),
+                    ...tempTrip.destinationCities,
+                    { city: newText, numberOfDays: 0 }
                   ]
                 })}
-                key={`dC-${idx}`}
               />
-            )}
-            <TextInput
-              style={styles.cityInput}
-              placeholder="Add destination"
-              value=""
-              onChangeText={(newText) => setTempTrip({
-                ...tempTrip,
-                destinationCities: [
-                  ...tempTrip.destinationCities,
-                  { city: newText, numberOfDays: 0 }
-                ]
-              })}
-            />
-            </View>
-          :
-          undefined
-        }
-      </TouchableOpacity>
+              </View>
+            :
+            undefined
+          }
+        </TouchableOpacity>
 
-      <View style={styles.card}>
-        <View style={styles.cardHeader}>
-          <Text style={styles.cardTitle}>When?</Text>
-          <Text style={styles.cardDescription}>{tripDateString(tempTrip)}</Text>
+        <View style={styles.card}>
+          <View style={styles.cardHeader}>
+            <Text style={styles.cardTitle}>When?</Text>
+            <Text style={styles.cardDescription}>{tripDateString(tempTrip)}</Text>
+          </View>
         </View>
-      </View>
 
-      <View style={styles.card}>
-        <View style={styles.cardHeader}>
-          <Text style={styles.cardTitle}>Who?</Text>
-          <Text style={styles.cardDescription}>{numberOfPeopleString(tempTrip)}</Text>
+        <View style={styles.card}>
+          <View style={styles.cardHeader}>
+            <Text style={styles.cardTitle}>Who?</Text>
+            <Text style={styles.cardDescription}>{numberOfPeopleString(tempTrip)}</Text>
+          </View>
         </View>
-      </View>
 
-      <View style={styles.card}>
-        <View style={styles.cardHeader}>
-          <Text style={styles.cardTitle}>Time in each destination?</Text>
+        <View style={styles.card}>
+          <View style={styles.cardHeader}>
+            <Text style={styles.cardTitle}>Time in each destination?</Text>
+          </View>
         </View>
-      </View>
+      </>
+      }
     </View>
   );
 };
@@ -160,6 +187,9 @@ const styles = StyleSheet.create({
     borderWidth: 1,
     fontSize: 14,
   },
+  cityOption: {
+    backgroundColor: 'red',
+  }
 });
 
 export default PreferencesScreen;
